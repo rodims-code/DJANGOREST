f4935e0502fb70ede6c52ef9757890d9a13279c8
rodims-code <ridrodims@hotmail.com>
2025-01-04 02:18:59 +0100

    Add detailed logging to api_view function

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,16 @@
+import json
 from django.shortcuts import render
 from django.http import JsonResponse
 # Create your views here.
 
 def api_view(request, *args, **kwargs) :
-    data = {
-        'name' : 'rodim\'s',
-        'language' : 'python',
-    }
-    return JsonResponse(data)
+    # request => HttpRequest
+    print (request.body)
+    data = json.loads(request.body)
+    print (data)
+    data['headers'] = dict(request.headers)
+    data['params'] = dict(request.GET)
+    data['post-data']= dict(request.POST)
+    print (request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data) # return JSON
--- client-api/basic.py
+++ client-api/basic.py
@@ -1,7 +1,7 @@
 import requests
 
 endpoint = 'http://127.0.0.1:8000/api/'
-response = requests.get(endpoint)
-print(response.text)
+response = requests.get(endpoint, params={'abc':123}, json={'name':'John', 'age':27})
+print(response.json())
 print(response.status_code)
 
