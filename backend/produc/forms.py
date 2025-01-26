9ab9c50fe5d8787c549889e06937ccfa7d6c611b
rodims-code <ridrodims@hotmail.com>
2025-01-26 23:52:07 +0100

    Installation du projet et application product

--- backend/produc/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-from .models import Product
-
-class ProductForm(forms.Form):
-    class Meta:
-        model = Product
-        fields = ['name', 'content', 'price']
--- client-api/basic.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-endpoint = 'http://127.0.0.1:8000/produc/'
-response = requests.get(endpoint)
-#print(response.json())
-print(response.status_code)
-
