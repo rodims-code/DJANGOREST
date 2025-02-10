5bfcc943e433043e2490e53ea714d0b4dbc9d47c
rodims-code <ridrodims@hotmail.com>
2025-02-10 14:51:02 +0100

    endpoint and api in json

--- client_api/basic.py
+++ client_api/basic.py
@@ -1,6 +1,24 @@
 import requests
 
-endpoint = "http://httpbin.org/status/product"
+endpoint = "http://httpbin.org/anything"
 
-response = requests.get(endpoint)
+response = requests.get(endpoint, data={"key": "value"})
 print(response.text)
+
+{
+  "args": {}, 
+  "data": "", 
+  "files": {}, 
+  "form": {}, 
+  "headers": {
+    "Accept": "*/*", 
+    "Accept-Encoding": "gzip, deflate", 
+    "Host": "httpbin.org", 
+    "User-Agent": "python-requests/2.32.3",
+    "X-Amzn-Trace-Id": "Root=1-67a9f921-3d5a541e4af20e4d7d057c11"
+  },
+  "json": null,
+  "method": "GET",
+  "origin": "102.129.82.97",
+  "url": "http://httpbin.org/anything"
+}
