dabf29f87a9909ae82aa8868b2e88e58f241f7b0
rodims-code <ridrodims@hotmail.com>
2025-02-12 16:04:46 +0100

    The first api avec django, le client python et le client navigateur

--- backend/rodimAPI/api/views.py
+++ backend/rodimAPI/api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+def api_view(request, *args, **kwargs):
+    data = {
+        'name': 'Rodim',
+        'age': 26,
+        'language': 'Python'
+    }
+    return JsonResponse(data)
--- client_api/basic.py
+++ client_api/basic.py
@@ -1,9 +1,9 @@
 import requests
 
-endpoint = "http://127.0.0.1:8000/"
+endpoint = "http://127.0.0.1:8000/api/"
 
 response = requests.get(endpoint)
-print(response.text)
+print(response.json())
 print(response.status_code)
 """ {
   "args": {}, 
