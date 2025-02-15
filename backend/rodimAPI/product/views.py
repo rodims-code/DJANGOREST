a31c64dcaace74dbaec0e787ae5421a7178377f9
rodims-code <ridrodims@hotmail.com>
2025-02-15 16:11:47 +0100

    Renvoyer les donneer dans la data base et le renvoyer en forme de json, l'introduction au api avec python django

--- backend/rodimAPI/product/views.py
+++ backend/rodimAPI/product/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from .models import Product
+from django.http import JsonResponse
+from django.forms.models import model_to_dict
 
 # Create your views here.
+def api_view(request):
+    #request != requests
+    #request is a Django object, requests is a Python module
+    query = Product.objects.all().order_by('?').first()
+    data = {}
+    if query:
+        #data['name'] = query.name
+        #data['description'] = query.description
+        #data['price'] = query.price
+        data = model_to_dict(query)
+        #serislization : convserion of complex data types to a format that can be stored or transmitted and reconstructed later
+    return JsonResponse(data)
--- client_api/basic.py
+++ client_api/basic.py
@@ -1,8 +1,8 @@
 import requests
 
-endpoint = "http://127.0.0.1:8000/api/"
+endpoint = "http://127.0.0.1:8000/product/"
 
-response = requests.get(endpoint, params={'abc':123}, json={'query': '{name, age, language}'})
+response = requests.get(endpoint)
 print(response.json())
 print(response.status_code)
 """ {
